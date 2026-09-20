f8f29b7777923110751bf702067400a66758c7f9
nityasharma26102007-max <nityasharma26102007@gmail.com>
2026-09-20 20:44:57 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -15,3 +15,12 @@ for i in range(10,100):
     if(i %2 != 0):
         print(i)
 
+#reverse no.
+num =int(input("enter the num value"))
+rev=0
+while num>0 :
+    rem=num%10
+    rev=rev*10+rem
+    num = num//10
+print(rev)
+
