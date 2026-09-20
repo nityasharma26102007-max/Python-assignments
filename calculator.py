4c898822aecb589b415300cdaee3eacc52b925cc
nityasharma26102007-max <nityasharma26102007@gmail.com>
2026-09-20 20:22:12 +0530

    Update pytonconditionstask.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+#Calculator assignment 1
+
+
 print("calculator")
 print("the operation whatever u want")
 num1 = int(input("enter the value num1"))
@@ -13,4 +16,29 @@ elif operator == '/':
     print("reslul",num1 / num2)
 else:
     print("it is invalid operation")
+
+#fever assignment 2
+
+temp = int(input("enter the digits of temp - "))
+print("enter ur body temperature", temp,"°C")
+if(temp > 102 ):
+    print("extremely hingh fevtemper")
+elif(102 > temp > 97):
+    print("mild fever")
+elif(97 > temp > 37):
+    print("normal temperature ")
+else:
+    print("consern to the doctor")
+
+#C to F
+
+
+celsius = float(input("enter the digit of celsius"))
+print("entered Celsius is",celsius,"°C")
+F = (celsius * 9/5) + 32 
+print(F)
+K=(celsius+273.15)
+print(K)
+
+
     
